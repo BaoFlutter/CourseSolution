ef6a001257f7d2d62d4486efb53f4e11ce528087
BaoFlutter <baoflutter@gmail.com>
2022-02-28 14:56:42 +0700

    update dart base

--- dart_base_excercise/bin/dart_base_excercise.dart
+++ dart_base_excercise/bin/dart_base_excercise.dart
@@ -1,17 +1,40 @@
 import 'package:dart_base_excercise/dart_base_excercise.dart' as dart_base_excercise;
 
-/*
-//0.Từ 1 đến 1000 in ra các số chẵn mà tổng các số đó không lớn hơn 400.
+
+
 
 void main(List<String> arguments) {
+  print("============== Bài tập 0 ==================");
+  //0.Từ 1 đến 1000 in ra các số chẵn mà tổng các số đó không lớn hơn 400.
+  printEvenNumberWithCondition(limitedNumber: 1000, maxSum: 400);
+  print("============== Bài tập 1 ==================");
+  //1. Từ. 1 đến 10000, Viết chương trình đếm xem có bao nhiêu chữ số chia hết cho 3 , bao nhiêu số chia hết cho 5
+  printHowManyNumbersDivisibleBy3And5(limitedNumber: 1000); 
+  print("============== Bài tập 2 ==================");
+  //2.Chomộtlist:Listlist5=[4,3,10,9,15,7,6,5,8]; Inratổngcácsốchiahếtcho3.
+  List<int> list5 = [4, 3, 10, 9, 15, 7, 6, 5, 8];
+  print("Tổng các số chia hết cho 3: ${getSumOfNumbersDevisible3(numberList:list5)}");
+  print("============== Bài tập 3 ==================");
+  // 3 : Tìm hotline 
+  Map classInformation = {
+    "id": 12,
+    "name": "Báo Flutter 1.2",
+    "description": "Lớp học lập trình Flutter, hot line: 0349582808"
+  };
+  String  description = classInformation['description'];
+  print(description.substring(description.length -10, description.length));
+
+}
+
+void printEvenNumberWithCondition({required int limitedNumber, required int maxSum}){
   int sum = 0;
   int tempSum = 0; 
   int count = 1;
-  print("các số chẵn mà tổng các số đó không lớn hơn 400 là:");
-  while (count <= 1000) {
+  print("các số chẵn mà tổng các số đó không lớn hơn $maxSum là:");
+  while (count <= limitedNumber) {
    if(count%2 == 0) { 
      sum += count;
-     if(sum > 4000) break;
+     if(sum > maxSum) break;
      print('$count');
      }
      tempSum = sum;
@@ -19,15 +42,11 @@ void main(List<String> arguments) {
   }
   print("Tổng các số đó là: $tempSum");
 }
-*/
 
-
-//1. Từ. 1 đến 10000, Viết chương trình đếm xem có bao nhiêu chữ số chia hết cho 3 , bao nhiêu số chia hết cho 5
-/*
-void main(List<String> arguments) {
+void printHowManyNumbersDivisibleBy3And5({required int limitedNumber}){
   int demChiaHetCho3 = 0;
   int demChiaHetCho5 = 0;
-  for (int i = 1; i <= 10000; i++) {
+  for (int i = 1; i <= limitedNumber; i++) {
     if (i % 3 == 0) {
       demChiaHetCho3++;
     }
@@ -38,40 +57,15 @@ void main(List<String> arguments) {
   print("==>có $demChiaHetCho3  chữ số chia hết cho 3\n");
   print("==>có $demChiaHetCho5 chữ số chia hết cho 5\n");
 }
-*/
-// 2.Chomộtlist:Listlist5=[4,3,10,9,15,7,6,5,8]; Inratổngcácsốchiahếtcho3.
-/*
-void main(List<String> arguments) {
+
+int getSumOfNumbersDevisible3({required List<int> numberList}){
   int sum = 0;
-  List<int> list5 = [4, 3, 10, 9, 15, 7, 6, 5, 8];
-  print("tổng các số chia hết cho 3 là:");
-  for (int number in list5) {
+  for (int number in numberList) {
     if (sum % 3 == 0) {
       sum += number;
     }
   }
-  print("$sum");
+  return sum; 
+  
 }
-*/
 
-/*
-//3. Cho thông tin biểu diễn dưới sạng sau :
-var classInformation = {
-“id”: 12,
-“name”: “Báo Flutter 1.2”,
-“description”: “ Lớp học lập trình Flutter, hot line: 0349582808”
-};
-Yêu cầu: In ra số hotline của lớp học trên.
- */
-//========== Bài giải =========
-/*
-void main(List<String> arguments) {
-  Map classInformation = {
-    "id": 12,
-    "name": "Báo Flutter 1.2",
-    "description": "Lớp học lập trình Flutter, hot line: 0349582808"
-  };
-  String  description = classInformation['description'];
-  print(description.substring(description.length -10, description.length));
-}
-*/
